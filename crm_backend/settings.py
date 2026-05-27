327f4ca6b38596093aeeb9be6ce7e42052ca71e0
seeja <seejavm@gmail.com>
2026-05-27 19:24:56 +0530

    render deployment setup

--- crm_backend/settings.py
+++ crm_backend/settings.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
+
 """
+import os
+import dj_database_url
+
 
 from pathlib import Path
 from dotenv import load_dotenv
@@ -26,9 +30,11 @@ load_dotenv(BASE_DIR / '.env')
 SECRET_KEY =os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -40,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'rest_framework.authtoken', 
     'corsheaders',
@@ -98,6 +105,7 @@ NEXT_PUBLIC_API_URL=os.getenv('NEXT_PUBLIC_API_URL')
 MIDDLEWARE = [
      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,15 +139,17 @@ WSGI_APPLICATION = 'crm_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':  os.getenv('DATABASE_NAME'),     # your database name
-        'USER': os.getenv('DATABASE_USER'),      # your PostgreSQL username
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),  # your PostgreSQL password
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+    
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME':  os.getenv('DATABASE_NAME'),     # your database name
+        # 'USER': os.getenv('DATABASE_USER'),      # your PostgreSQL username
+        # 'PASSWORD': os.getenv('DATABASE_PASSWORD'),  # your PostgreSQL password
+        # 'HOST': os.getenv('DATABASE_HOST'),
+        # 'PORT': os.getenv('DATABASE_PORT'),
     }
-}
+
 
 
 # Password validation
@@ -177,7 +187,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
