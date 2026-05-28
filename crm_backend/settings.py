32e748ba991ce87ecbf04c779fa94d0c7f26f410
seeja <seejavm@gmail.com>
2026-05-28 22:53:23 +0530

    print in settings

--- crm_backend/settings.py
+++ crm_backend/settings.py
@@ -97,7 +97,7 @@ TWILIO_PHONE_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')     # ← your gmail
